97ea48f251d31a0cbb240c2a7c676160f4eedab1
david sastre <davidsastresas@gmail.com>
2020-11-22 20:53:25 +0100

    TelemetryGroundUnit: add properties, getters and signals:
    With this commit the qml frontend can
    already read correctly the status of
    ground unit

--- src/comm/TelemetryGroundUnit.cc
+++ src/comm/TelemetryGroundUnit.cc
@@ -17,5 +17,40 @@ void TelemetryGroundUnit::setToolbox(QGCToolbox *toolbox)
 }
 
 void TelemetryGroundUnit::_radioStatusInfo(mavlink_radio_status_t radiostatus) {
-    qDebug() << "radio status received!";
+
+    _sbd_quality = radiostatus.rxerrors;
+    _sms_quality1 = radiostatus.rssi;
+    _sms_quality2 = radiostatus.remrssi;
+    _sms_quality3 = radiostatus.txbuf;
+    _status_bitmask = radiostatus.noise;
+    _link_bitmask = radiostatus.remnoise; // always 0, depends on the aircraft
+
+    if ( _status_bitmask & (1 << 0) ) // Low voltage happening
+        _lowVolt = true;
+    else
+        _lowVolt = false;
+
+    if ( _status_bitmask & (1 << 2) ) // throttling happening
+        _currentlyThrottled = true;
+    else
+        _currentlyThrottled = false;
+    
+    if ( _status_bitmask & (1 << 4) ) // low voltage ocurred
+        _ocurredLowVolt = true;
+    else
+        _ocurredLowVolt = false;
+    
+    if ( _status_bitmask & (1 << 6) ) // throttling ocurred
+        _ocurredThrottled = true;
+    else
+        _ocurredThrottled = false;
+
+    emit onSbdQualityChanged();
+    emit onSmsQuality1Changed();
+    emit onSmsQuality2Changed();
+    emit onSmsQuality3Changed();
+    emit onCurrentlyThrottledChanged();
+    emit onOcurredThrottledChanged();
+    emit onLowVoltChanged();
+    emit onOcurredLowVoltChanged();
 }
--- src/comm/TelemetryGroundUnit.h
+++ src/comm/TelemetryGroundUnit.h
@@ -10,6 +10,23 @@ class TelemetryGroundUnit: public QGCTool
     Q_OBJECT
 
 public:
+    Q_PROPERTY(int sbdQuality   READ sbdQuality   NOTIFY onSbdQualityChanged);
+    Q_PROPERTY(int smsQuality1  READ smsQuality1  NOTIFY onSmsQuality1Changed);
+    Q_PROPERTY(int smsQuality2  READ smsQuality2  NOTIFY onSmsQuality2Changed);
+    Q_PROPERTY(int smsQuality3  READ smsQuality3  NOTIFY onSmsQuality3Changed);
+    Q_PROPERTY(bool currentlyThrottled   READ currentlyThrottled   NOTIFY onCurrentlyThrottledChanged);
+    Q_PROPERTY(bool ocurredThrottled     READ ocurredThrottled     NOTIFY onOcurredThrottledChanged);
+    Q_PROPERTY(bool lowVolt              READ lowVolt              NOTIFY onLowVoltChanged);
+    Q_PROPERTY(bool ocurredLowVolt       READ ocurredLowVolt       NOTIFY onOcurredLowVoltChanged);
+
+    int sbdQuality(void)           { return _sbd_quality; }
+    int smsQuality1(void)          { return _sms_quality1; }
+    int smsQuality2(void)          { return _sms_quality2; }
+    int smsQuality3(void)          { return _sms_quality3; }
+    bool currentlyThrottled(void)  { return _currentlyThrottled; }
+    bool ocurredThrottled(void)    { return _ocurredThrottled; }
+    bool lowVolt(void)             { return _lowVolt; }
+    bool ocurredLowVolt(void)      { return _ocurredLowVolt; }    
 
     TelemetryGroundUnit(QGCApplication* app, QGCToolbox* toolbox);
     ~TelemetryGroundUnit();
@@ -21,4 +38,30 @@ private slots:
     void _radioStatusInfo(mavlink_radio_status_t radiostatus);
 
     friend class MAVLinkProtocol;
+
+signals:
+
+    void onSbdQualityChanged(void);
+    void onSmsQuality1Changed(void);
+    void onSmsQuality2Changed(void);
+    void onSmsQuality3Changed(void);
+    void onCurrentlyThrottledChanged(void);
+    void onOcurredThrottledChanged(void);
+    void onLowVoltChanged(void);
+    void onOcurredLowVoltChanged(void);
+
+private:
+
+    // raw groundpi radio status message
+    int _sbd_quality = 0;
+    int _sms_quality1 = 0;
+    int _sms_quality2 = 0;
+    int _sms_quality3 = 0;
+    uint8_t _status_bitmask = 255; // uninitialized value
+    uint8_t _link_bitmask = 0; // depends only on each aircraft
+
+    bool _currentlyThrottled = false;
+    bool _ocurredThrottled = false;
+    bool _lowVolt = false;
+    bool _ocurredLowVolt = false;
 };
